a46b271885bbf98b1c1dd84b3a4f7d8bff6480bb
Sukram0815 <mb@blue-danube-it.de>
2013-10-21 13:26:31 +0200

    Removed debugger...

--- app/controllers/odata/Testing.controller.js
+++ app/controllers/odata/Testing.controller.js
@@ -1,14 +1,15 @@
-sap.ui.controller("de.blue_danube_it.blueui5.controllers.odata.Testing", {
-// Connection success flag.
+sap.ui.controller("de.blue_danube_it.blueui5.controllers.odata.Testing", {					// Connection
+																							// success
+																							// flag.
 success : false,
 // Reverse proxy setup to
 // http://services.odata.org/Northwind/Northwind.svc/
 sServiceUrl : "/northwind/",
 /**
- * Called when a controller is instantiated and its View controls
- * (if available) are already created. Can be used to modify the
- * View before it is displayed, to bind event handlers and do other
- * one-time initialization.
+ * Called when a controller is instantiated and its View
+ * controls (if available) are already created. Can be used
+ * to modify the View before it is displayed, to bind event
+ * handlers and do other one-time initialization.
  * 
  * @memberOf app.controllers.odata.Testing
  */
@@ -36,28 +37,52 @@ onInit : function() {
 					schema1);
 
 			this.getView().setModel(oModel, "results");
-			this.getView().setModel(oSchemaModel0, "schema0");
-			this.getView().setModel(oSchemaModel1, "schema1");
+			this.getView().setModel(oSchemaModel0,
+					"schema0");
+			this.getView().setModel(oSchemaModel1,
+					"schema1");
 
 			this.success = true;
 		}
-	} catch (eException) {
+	} catch (oException) {
 	}
 
 	this.setUpPretyContent();
 },
+onBind : function() {
+	if (!this.checkConnection()) return;
+	
+	// do Binding!
+},
+
+onUnBind : function() {
+	if (!this.checkConnection()) return;
+	// do unBinding!
+},
+
+checkConnection:function(){
+	if (!this.success) {
+		sap.ui.commons.MessageBox
+			.show(
+			"You triggered onBind. Unfortunatelly connection to oData service is not setup propperly.",
+			"ERROR", "Connettion failed",
+			"CLOSE", null, "CLOSE");
+	}
+	return this.success;
+},
 
 setUpPretyContent : function() {
-	var oContentModel = new sap.ui.model.json.JSONModel(this);
+	var oContentModel = new sap.ui.model.json.JSONModel(
+			this);
 	this.getView().setModel(oContentModel, 'pretty');
 	// oContentModel.loadData(this);
 	// debugger;
 },
 
 /**
- * Similar to onAfterRendering, but this hook is invoked before the
- * controller's View is re-rendered (NOT before the first rendering!
- * onInit() is used for that one!).
+ * Similar to onAfterRendering, but this hook is invoked before
+ * the controller's View is re-rendered (NOT before the first
+ * rendering! onInit() is used for that one!).
  * 
  * @memberOf app.controllers.odata.Testing
  */
@@ -65,10 +90,10 @@ setUpPretyContent : function() {
 //
 // },
 /**
- * Called when the View has been rendered (so its HTML is part of the
- * document). Post-rendering manipulations of the HTML could be done
- * here. This hook is the same one that SAPUI5 controls get after being
- * rendered.
+ * Called when the View has been rendered (so its HTML is part
+ * of the document). Post-rendering manipulations of the HTML
+ * could be done here. This hook is the same one that SAPUI5
+ * controls get after being rendered.
  * 
  * @memberOf app.controllers.odata.Testing
  */
